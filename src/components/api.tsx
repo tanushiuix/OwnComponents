a96b5358a2477351ed1b7b23b930119aeafc08c4
tanushiuix <tanushi@uixlabs.in>
2024-07-04 16:14:59 +0530

    some files removed

--- src/components/api.tsx
+++
@@ -1,7 +0,0 @@
-export const fetchData = (): Promise<string> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Data fetched successfully!");
-    }, 7000);
-  });
-};
